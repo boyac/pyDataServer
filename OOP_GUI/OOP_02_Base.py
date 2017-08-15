65287e9a14855ffdb0d7711a6c1c9c3cd14ab192
Boya Chiou <chinawhitewhy@gmail.com>
2017-08-15 15:26:16 +0800

    Create OOP_02_Base.py

---
+++ OOP_GUI/OOP_02_Base.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author: boyac
+# @Date:   2017-08-15 14:58:28
+# @Last Modified by:   boyac
+# @Last Modified time: 2017-08-15 15:06:12
+
+class BaseClass(object):
+	def __init__(self):
+		self.x = 5
+
+	def printHam(self):
+		print 'ham'
